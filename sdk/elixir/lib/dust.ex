8f615f47c27bfc3112cfc00a44b82fcf4105c132
James Tippett <james.tippett@gmail.com>
2026-04-13 11:43:51 +0700

    feat(sdk): expose Dust.entry/2

--- sdk/elixir/lib/dust.ex
+++ sdk/elixir/lib/dust.ex
@@ -16,6 +16,7 @@ defmodule Dust do
   end
 
   defdelegate get(store, path), to: Dust.SyncEngine
+  defdelegate entry(store, path), to: Dust.SyncEngine
   defdelegate put(store, path, value), to: Dust.SyncEngine
   defdelegate delete(store, path), to: Dust.SyncEngine
   defdelegate merge(store, path, map), to: Dust.SyncEngine
--- sdk/elixir/test/dust_test.exs
+++ sdk/elixir/test/dust_test.exs
@@ -1,3 +1,21 @@
 defmodule DustTest do
   use ExUnit.Case
+
+  setup do
+    start_supervised!({Registry, keys: :unique, name: Dust.SyncEngineRegistry})
+    start_supervised!({Registry, keys: :unique, name: Dust.ConnectionRegistry})
+
+    {:ok, _pid} =
+      Dust.SyncEngine.start_link(
+        store: "test/store",
+        cache: {Dust.Cache.Memory, []}
+      )
+
+    :ok
+  end
+
+  test "Dust.entry/2 delegates to SyncEngine" do
+    :ok = Dust.SyncEngine.seed_entry("test/store", "a", 1, "integer")
+    assert {:ok, %Dust.Entry{path: "a", value: 1, revision: _}} = Dust.entry("test/store", "a")
+  end
 end
