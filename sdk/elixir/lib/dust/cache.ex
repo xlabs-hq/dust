0b33ffb1b130b48e2c55f33d418c34f605336f81
James Tippett <james.tippett@gmail.com>
2026-04-13 11:34:44 +0700

    feat(sdk): add read_entry/3 to Dust.Cache behaviour

--- sdk/elixir/lib/dust/cache.ex
+++ sdk/elixir/lib/dust/cache.ex
@@ -1,5 +1,7 @@
 defmodule Dust.Cache do
   @callback read(target :: term(), store :: String.t(), path :: String.t()) :: {:ok, term()} | :miss
+  @callback read_entry(target :: term(), store :: String.t(), path :: String.t()) ::
+              {:ok, {value :: term(), type :: String.t(), seq :: integer()}} | :miss
   @callback read_all(target :: term(), store :: String.t(), pattern :: String.t()) :: [{String.t(), term()}]
   @callback write(target :: term(), store :: String.t(), path :: String.t(), value :: term(), type :: String.t(), seq :: integer()) :: :ok
   @callback write_batch(target :: term(), store :: String.t(), entries :: list()) :: :ok
@@ -9,5 +11,5 @@ defmodule Dust.Cache do
   @callback browse(target :: term(), store :: String.t(), opts :: keyword()) ::
               {[{String.t(), term(), String.t(), integer()}], term() | nil}
 
-  @optional_callbacks [count: 2, browse: 3]
+  @optional_callbacks [count: 2, browse: 3, read_entry: 3]
 end
