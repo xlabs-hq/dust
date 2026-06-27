dcd01f9fc43383cad82d3509efe59a30debf8947
James Tippett <james.tippett@gmail.com>
2026-06-27 14:32:29 +0700

    Release dustlayer_ecto 0.1.1

--- sdk/elixir_ecto/mix.exs
+++ sdk/elixir_ecto/mix.exs
@@ -1,7 +1,7 @@
 defmodule DustEcto.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/xlabs-hq/dust/tree/master/sdk/elixir_ecto"
 
   def project do
